f10ac4f181e3b41d50bd1312769fa794cfe39128
guilhermeneiva <143911807+guilhermeneiva@users.noreply.github.com>
2024-07-29 18:45:29 -0300

    fiz o jogo parar quando o mario encostar no cano e mudei tamanho do cano

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 const mario = document.querySelector('.mario')
+const cano = document.querySelector('.cano')
 
 function jump() {
   mario.classList.add('jump')
 
-  setTimeout(function () {
+  setTimeout(() => {
     mario.classList.remove('jump')
-  }, 500)
+  }, 500);
 }
 
+setInterval(() => {
+
+  const posicaoCano = cano.offsetLeft;
+  const posicaoMario = +getComputedStyle(mario).bottom.replace('px', '');
+  console.log(posicaoMario);
+
+  if (posicaoCano <= 120 && posicaoCano > 0 && posicaoMario < 80) {
+    cano.style.animation = 'none'
+    cano.style.left = `${posicaoCano}px`
+
+    mario.style.animation = 'none'
+    mario.style.bottom = `${posicaoMario}px`
+
+  }
+
+}, 10);
+
+
+
 document.addEventListener('keydown', jump)
+
